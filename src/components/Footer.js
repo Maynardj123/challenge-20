b39f80e364c8948e6d2790d85d3b19e70c6eef16
jacob m <jacobamaynard@gmail.com>
2023-03-29 07:59:47 -0500

    added footer with clickable links

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="w-100 my-auto bg-secondary p-4">
+            <div className="container text-center mb-5">
+                <a href="https://github.com/Maynardj123">
+                    <img src="..\styles\25231 (1).png"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/jacob-maynard-2b1426172/">
+                    <img src="..\styles\linkedin-logo-3.png"></img>
+                </a>
+                <a href="https://www.freecodecamp.org/Maynardj123">
+                    <img src="..\styles\5847f91ccef1014c0b5e48b8.png"></img>
+                </a>
+             
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1>Maynard</h1>
+      <h1>Jacob Maynard</h1>
     </header>
   );
 }
